1a357b9f9b944282cfa3b8eb032693c51bebdd34
Praveen8995 <45147158+Praveen8995@users.noreply.github.com>
2019-11-06 03:40:41 +0530

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -15,16 +15,24 @@ request.onload = function() {
 
       const h1 = document.createElement('h1')
       // h1.textContent = customer.estimate_id
-
+      const button = document.createElement('button',"width=300,height=200")
+      button.setAttribute('class','button')
       const p = document.createElement('p')
       // p.textContent = customer.city_type
       // p.textContent = customer.items.rooms.living_room.center_table
+      button.innerHTML = 'view details'
+      button.onclick = function(){
+        alert(`living room\ncenter table : ${customer.items.rooms.living_room.center_table}\ncomputer_table : ${customer.items.rooms.living_room.computer_table} `);
+        return false;
+      };
+      // button = customer.items.rooms.living_room.center_table
       card.innerHTML = '<i class="fa fa-home" style="color:grey;font-size:30px;margin-top:25px;"></i>' + '<a href="#" style="position:relative;top:-30px;left:-25px;">'+ customer.estimate_id + '</a>' + '<span style="position:relative;left:-50px; top:-1px;">'+ customer.select_city + '</span>'+ '<span style="position:relative;left:-145px; top:20px;"> Order date:' + customer.order_date + '</span>' 
-                        + '<span style="position:relative;left:50px; top:-5px;color:grey;">' + customer.moving_from + '</span>' + '<span style="position:relative;left:100px; top:-20px;color:grey;">' + customer.moving_on +  '</span>' + '<button type="button" data-toggle="collapse" data-target="#demo" style="background-color:blue;height:40px;width:100px;position:relative; top: 10px; left: 15px;">view details</button>' +'<span style="position:relative;left:150px; top:-5px;">' + customer.moving_to + '</span>' + '<span style="position:relative;left:190px; top:25px;">' + customer.custom_status + '</span>';
+                        + '<span style="position:relative;left:50px; top:-5px;color:grey;">' + customer.moving_from + '</span>' + '<span style="position:relative;left:100px; top:-20px;color:grey;">' + customer.moving_on + '</span>'+'<span style="position:relative;left:150px; top:-5px;">' + customer.moving_to + '</span>' + '<span style="position:relative;left:190px; top:25px;">' + customer.custom_status + '</span>';
 
       container.appendChild(card)
       card.appendChild(h1)
       card.appendChild(p)
+      card.appendChild(button)
     })
   } else {
     const errorMessage = document.createElement('marquee')
@@ -34,5 +42,6 @@ request.onload = function() {
 }
 
 request.send()
+
 
 
